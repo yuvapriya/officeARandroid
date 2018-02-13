4709b1ce5c8ee628081fba212fa5a148c1d2b3d5
yumanoha <m.yuvapriya@gmail.com>
2018-02-13 15:35:52 -0800

    ADAL integration

--- app/src/main/java/com/example/yumanoha/officeARandroid/MainActivity.java
+++ app/src/main/java/com/example/yumanoha/officeARandroid/MainActivity.java
@@ -16,15 +16,54 @@ import com.google.android.gms.common.api.CommonStatusCodes;
 import com.google.android.gms.vision.barcode.Barcode;
 import com.example.yumanoha.officeARandroid.barcode.BarcodeCaptureActivity;
 
+import com.microsoft.aad.adal.AuthenticationContext;
+import com.microsoft.aad.adal.AuthenticationResult;
+import com.microsoft.aad.adal.AuthenticationCallback;
+import com.microsoft.aad.adal.PromptBehavior;
+
+import static com.microsoft.aad.adal.AuthenticationConstants.OAuth2.AUTHORITY;
+
 public class MainActivity extends AppCompatActivity {
 
     private static final int BARCODE_READER_REQUEST_CODE = 1;
     private TextView mResultTextView;
 
+    private AuthenticationContext authenticationContext;
+    private static final String CLIENT_ID = "da1a049e-1bf8-407c-8e76-bac9ff2f34dd";
+    private static final String TENANT_ID = "72f988bf-86f1-41af-91ab-2d7cd011db47";
+    private static final String REDIRECT_URI_BASE = "ms-appx-web://Microsoft.AAD.BrokerPlugin";
+    private static final String REDIRECT_URI = REDIRECT_URI_BASE + "/" + CLIENT_ID;
+    private static final String GRAPH_RESOURCE = "https://graph.microsoft.com";
+    private static final String AUTHORITY = "https://login.microsoftonline.com/" + TENANT_ID;
+    //private static final String LOG_TAG = "AUTH";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Create the authentication context.
+        authenticationContext = new AuthenticationContext(MainActivity.this,
+                AUTHORITY, true);
+
+        // Acquire tokens using necessary UI.
+        authenticationContext.acquireToken(MainActivity.this, GRAPH_RESOURCE, CLIENT_ID, REDIRECT_URI,
+        PromptBehavior.Always, new AuthenticationCallback<AuthenticationResult>() {
+            @Override
+            public void onSuccess(AuthenticationResult result) {
+                String idToken = result.getIdToken();
+                String accessToken = result.getAccessToken();
+
+                // Print tokens.
+                /*Log.d(LOG_TAG, "ID Token: " + idToken);
+                Log.d(LOG_TAG, "Access Token: " + accessToken);*/
+            }
+
+            @Override
+            public void onError(Exception exc) {
+                // TODO: Handle error
+            }
+        });
+
         /*Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);*/
         mResultTextView = findViewById(R.id.result_textview);
